16fe3d4fbedda30baea5a8e151af4c629eca2c75
h3rmit <76179581+h3rmitpurpl33@users.noreply.github.com>
2021-08-30 15:03:55 +0300

    Update binary_search_tree_sample.c

--- binary_search_tree_sample.c
+++ binary_search_tree_sample.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct node {
     int key;
     struct node *left;
@@ -15,6 +18,10 @@ void delete_tree(node *p);
 
 node *find_node(node *p, int key);
 
+void show_preorder(node *p);
+
+void show_postorder(node *p);
+
 int main() {
     int sel, key;
     node *root;
@@ -25,7 +32,9 @@ int main() {
         printf("1. Add Node\n");
         printf("2. Show Tree\n");
         printf("3. Find Node\n");
-        printf("4. Exit\n");
+        printf("4. Show preorder\n");
+        printf("5. Show postorder\n");
+        printf("6. Exit\n");
 
         printf("enter choice :  ");
         scanf("%d", &sel);
@@ -47,12 +56,26 @@ int main() {
                 scanf("%d", &key);
                 root = find_node(root, key);
                 if (root != NULL) {
-                    root->key = key;
                     printf("found key %d", root->key);
                 } else {
                     printf("\nkey is not found\n");
                 }
                 break;
+
+            case 4:
+                if (root != NULL) {
+                    show_preorder(root);
+                } else {
+                    printf("list is empty\n");
+                }
+                break;
+            case 5:
+                if (root != NULL) {
+                    show_postorder(root);
+                } else {
+                    printf("list is empty\n");
+                }
+
 
             default:
                 printf("\nWrong choice\n");
@@ -116,4 +139,24 @@ node *find_node(node *p, int key) {
         }
     }
     return NULL;
+}
+
+void show_preorder(node *p) {
+    if (p == NULL) {
+        return;
+    }
+    printf("%d ", p->key);
+    show_preorder(p->left);
+    show_preorder(p->right);//η διαφορα ειναι η κληση της prinft()
+    //κληση πριν την show_preorder δειχνει τις ριζες με τον τροπο που τις εισαγαμε
+    //κληση μετα την show_postorder τις δειχνει αντιστροφα
+}
+
+void show_postorder(node *p) {
+    if (p == NULL) {
+        return;
+    }
+    show_postorder(p->left);
+    show_postorder(p->right);
+    printf("%d ", p->key);
 }
